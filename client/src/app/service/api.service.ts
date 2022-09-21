6c800cb42104b7bcc9afccadc000a5c3417fd2ad
JalelBarhoumi <jalel.barhoumi@esprit.tn>
2022-09-21 12:01:35 +0100

    commit day-1

--- client/src/app/service/api.service.ts
+++ client/src/app/service/api.service.ts
@@ -234,9 +234,8 @@ REST_APIAE: string = 'http://localhost:3001/affectationenseignant';
 
 
   putEnseignant2(data : any){
-    console.log("1111111111111111111");
-    let API_URL = `http://localhost:3001/affectationenseignant/updatee`;
-    return this.http.put<any>(API_URL,data);
+
+    return this.http.put<any>(`${this.REST_APIAE}/update`,data);
 
   }
 
